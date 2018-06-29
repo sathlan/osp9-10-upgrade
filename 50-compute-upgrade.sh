c9f75707cadc6857ca09a2e9ba283994a02b0fc3
Sofer Athlan-Guyot <chem@sathlan.org>
2018-06-29 13:00:53 +0200

    Make compute upgrade in dependant of directory it's run.

--- 50-compute-upgrade.sh
+++ 50-compute-upgrade.sh
@@ -1,5 +1,5 @@
 ./timestamp-ping.sh 50-compute-upgrade-$i-begin
-for i in $(cat compute-ip.txt); do
+for i in $(cat ~/compute-ip.txt); do
     ssh heat-admin@$i
     nohup upgrade-non-controller.sh --upgrade $i > ~/compute-${i}-upgrade.log &
 done
