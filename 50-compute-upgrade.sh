ce9c38906c7ffe2957587fe5df44c5fed3b3f0ba
Sofer Athlan-Guyot <chem@sathlan.org>
2018-06-29 13:11:04 +0200

    Fix compute upgrade.

--- 50-compute-upgrade.sh
+++ 50-compute-upgrade.sh
@@ -1,7 +1,16 @@
-./timestamp-ping.sh 50-compute-upgrade-$i-begin
+#!/usr/bin/bash
+
+set -eux
+
+. ${HOME}/stackrc
+
+./timestamp-ping.sh 50-compute-upgrade-begin
 for i in $(cat ~/compute-ip.txt); do
     grep -q ${i} ~/.ssh/known_hosts || ssh-keyscan -t rsa ${i} >> ~/.ssh/known_hosts
-    nohup upgrade-non-controller.sh --upgrade $i > ~/compute-${i}-upgrade.log &
 done
-wait
-./timestamp-ping.sh 50-compute-upgrade-$i-end
+for i in $(jq -r '.[] | select(.Name | contains("compute"))|.Name' ~/server.json); do
+    echo "UPGRADING $i"
+    upgrade-non-controller.sh --upgrade $i
+    echo "DONE $i"
+done
+./timestamp-ping.sh 50-compute-upgrade-end
