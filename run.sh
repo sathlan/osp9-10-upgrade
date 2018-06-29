3b4411fe3f202c1553ddb0687800c2fecac925ed
Sofer Athlan-Guyot <chem@sathlan.org>
2018-06-29 11:54:46 +0200

    Fix the run script.

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ set -eux
 script="${1:?You must provide the script to run}"
 log=${script}.log
 cpt=1
-while [ cpt -le 20 -a -e $log ] ; do
+while [ $cpt -le 20 -a -e $log ] ; do
     log=${script}.${cpt}.log
     cpt=$((cpt+1))
 done
