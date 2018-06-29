90c6777c76cd8e422cac89a5d475063b31d7f0de
Sofer Athlan-Guyot <chem@sathlan.org>
2018-06-29 13:05:35 +0200

    Add compute node ssh fingerprint.

--- 50-compute-upgrade.sh
+++ 50-compute-upgrade.sh
@@ -1,6 +1,6 @@
 ./timestamp-ping.sh 50-compute-upgrade-$i-begin
 for i in $(cat ~/compute-ip.txt); do
-    ssh heat-admin@$i
+    grep -q ${i} ~/.ssh/known_hosts || ssh-keyscan -t rsa ${i} >> ~/.ssh/known_hosts
     nohup upgrade-non-controller.sh --upgrade $i > ~/compute-${i}-upgrade.log &
 done
 wait
