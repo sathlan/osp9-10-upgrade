0da99cc1bc7900c60e1358fddad14771f00b8eae
Sofer Athlan-Guyot <chem@sathlan.org>
2018-07-25 17:07:01 +0200

    Various fix to prep.

--- bzs/1599368/prep.sh
+++ bzs/1599368/prep.sh
@@ -13,23 +13,30 @@ echo "${ceph_ip} choosen"
 ${SSH} heat-admin@${ceph_ip} sudo sed -i.orig -e 's/SELINUX=permissive/SELINUX=disabled/' /etc/sysconfig/selinux
 ${SSH} heat-admin@${ceph_ip} sudo getenforce
 ${SSH} heat-admin@${ceph_ip} rpm -qa | grep ceph-selinux
-${SSH} heat-admin@${ceph_ip} sudo reboot
+${SSH} heat-admin@${ceph_ip} sudo reboot || true
+
+# Wait for the server to die.
+sleep 10
 
 max_wait=120
-cur_wait=0
+current_wait=0
 while ! ping -c 2 -W 1 ${ceph_ip}; do
     if [ $current_wait -ge $max_wait ]; then
         echo "Waited more than 2 minutes for the instance to come back, giving up."
         exit 1
     fi
     sleep 1
+    current_wait=$((current_wait + 1))
 done
+
+# Wait for ssh to be there again.
+sleep 20
 
 ${SSH} heat-admin@${ceph_ip} sudo du -sh /var/lib/ceph/osd/ceph-1/
 ${SSH} heat-admin@${ceph_ip} sudo find /var/lib/ceph/osd/ceph-1/ -type f -ls
 
-curl -v 'https://code.engineering.redhat.com/gerrit/changes/143420/revisions/latest/patch?download' \
+curl -k -v 'https://code.engineering.redhat.com/gerrit/changes/143420/revisions/current/patch?download' | \
     base64 -d | \
-    sudo patch  -d /usr/share/openstack-tripleo-heat-templates/ -p1
+    sudo patch -d /usr/share/openstack-tripleo-heat-templates/ -p1
 
 echo "NOW RUN INIT."
